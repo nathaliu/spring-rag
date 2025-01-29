84b58c21d6e521ca044b8f68a8134257197f902b
Nathalie Liu <liunathalie@hotmail.fr>
2025-01-29 13:29:12 +0100

    update Document.java

--- src/main/java/fr/efrei/spring_rag/domain/Document.java
+++ src/main/java/fr/efrei/spring_rag/domain/Document.java
@@ -1,8 +1,14 @@
 package fr.efrei.spring_rag.domain;
 
-import jakarta.persistence.Entity;
-
-@Entity
+import jakarta.persistence.*;
 public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+    private String title;
+    private String description;
+    private String author;
+    private String publisher;
 
 }
